c1852e81e4cb17f066145c536d9d29b784b12b3d
Sebastian-8a <s.ochoa@udea.edu.co>
2023-09-17 09:57:41 -0500

    Modificación Tinkercad y avance

--- ProyectoParcial1Info2/main.cpp
+++ ProyectoParcial1Info2/main.cpp
@@ -4,7 +4,15 @@
 int main(int argc, char *argv[])
 {
     int resultadoBinario = 0;
-    int arreglo[8]={1,0,1,0,1,0,1,0};
+    int arreglo[8]={1,0,0,0,1,1,1,0};
+    int matriz[8][8];
+
+    for (int j = 0; j < 8; j++){
+
+        resultadoBinario = binarioADecimal(matriz[j]) ;
+
+
+    }
 
     resultadoBinario = binarioADecimal(arreglo) ;
     cout << resultadoBinario;
